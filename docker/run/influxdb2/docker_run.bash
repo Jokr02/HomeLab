0c266a20cb5bf6bf065306578d1f43ba59fd2197
Johannes <67365748+Jokr02@users.noreply.github.com>
2025-06-18 21:11:47 +0200

    Create docker_run.bash

---
+++ docker/run/influxdb2/docker_run.bash
@@ -0,0 +1,17 @@
+docker run -d -p 8086:8086 \
+
+  -v "$PWD/data:/var/lib/influxdb2" \
+
+  -v "$PWD/config:/etc/influxdb2" \
+
+  -e DOCKER_INFLUXDB_INIT_MODE=setup \
+
+  -e DOCKER_INFLUXDB_INIT_USERNAME=username \
+
+  -e DOCKER_INFLUXDB_INIT_PASSWORD=password \
+
+  -e DOCKER_INFLUXDB_INIT_ORG=organisation \
+
+  -e DOCKER_INFLUXDB_INIT_BUCKET=bucket \
+
+  influxdb:2
